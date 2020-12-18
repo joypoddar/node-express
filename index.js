a4dca36e1059b6adcda0aeeabb42d3c36d60c6b0
Joy Poddar <joypoddar.sd@gmail.com>
2020-12-18 21:06:10 +0530

    Assignment 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const leaderRouter = require("./routes/leaderRouter");
+const promoRouter = require("./routes/promoRouter");
 const dishRouter = require("./routes/dishRouter");
 const hostname = "localhost";
 const port = "3000";
@@ -12,6 +14,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use(express.static(__dirname + "/public"));
 
